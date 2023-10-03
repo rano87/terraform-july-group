807e356187f3f1ba19f4bcfef1df8978f2294a85
rano87 <ranonematova87@gmail.com>
2023-10-03 22:49:51 +0000

    add new folder

--- class3/VPC/security.tf
+++ class3/VPC/security.tf
@@ -2,6 +2,8 @@ resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
   vpc_id = aws_vpc.main.id
+  
+
   ingress {
     description      = "TLS from VPC"
     from_port        = 22
@@ -27,8 +29,9 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["0.0.0.0/0"]
 
   }
-  tags = 
 }
+ 
+
 
     
 
